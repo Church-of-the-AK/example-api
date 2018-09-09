99e10e521fe4f4bd40f389640fda19036ac8f5cc
Jason Bothel <shadowmanguyyt@gmail.com>
2018-09-09 13:01:07 -0400

    Use latest version of database, make errors better.

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,6 +1,6 @@
 import { Application } from 'express'
 import { getRepository } from 'typeorm'
-import { User, UserLevel, UserBalance, UserLinks } from 'machobot-database'
+import { User, UserLevel, UserBalance, UserLinks, UserSteamLinks, UserGithubLinks } from 'machobot-database'
 import * as config from '../config/config'
 import * as fs from 'fs'
 import * as jwt from 'jsonwebtoken'
@@ -10,6 +10,8 @@ export async function UserRoutes (app: Application) {
   const userLevelRepository = getRepository(UserLevel)
   const userBalanceRepository = getRepository(UserBalance)
   const userLinksRepository = getRepository(UserLinks)
+  const userSteamLinksRepository = getRepository(UserSteamLinks)
+  const userGithubLinksRepository = getRepository(UserGithubLinks)
 
   app.get('/users', async (req, res) => {
     const users = await userRepository.find()
@@ -18,7 +20,7 @@ export async function UserRoutes (app: Application) {
   })
 
   app.get('/users/:id', async (req, res) => {
-    const user = await userRepository.findOne(req.params.id, { relations: [ 'balance', 'level', 'links' ] })
+    const user = await userRepository.findOne(req.params.id, { relations: [ 'balance', 'level' ] })
 
     res.send(user)
   })
@@ -59,7 +61,7 @@ export async function UserRoutes (app: Application) {
 
     if (code !== config.code) {
       res.statusCode = 401
-      return res.send({ code: 401, message: 'invalid code' })
+      return res.send({ error: 'Invalid code.' })
     }
 
     const user = new User(userReq)
@@ -67,8 +69,6 @@ export async function UserRoutes (app: Application) {
     user.level = new UserLevel()
     user.balance = new UserBalance()
     user.links = new UserLinks()
-
-    user.links.steamId = ''
 
     const response = await userRepository.save(user).catch(error => {
       console.log(error)
@@ -85,7 +85,7 @@ export async function UserRoutes (app: Application) {
 
     if (code !== config.code) {
       res.statusCode = 401
-      return res.send({ code: 401, message: 'invalid code' })
+      return res.send({ error: 'Invalid code.' })
     }
 
     const response = await userRepository.save(user).catch(error => {
@@ -101,7 +101,7 @@ export async function UserRoutes (app: Application) {
 
     if (code !== config.code) {
       res.statusCode = 401
-      return res.send({ code: 401, message: 'invalid code' })
+      return res.send({ error: 'Invalid code.' })
     }
 
     let responses: any[] = []
@@ -144,7 +144,7 @@ export async function UserRoutes (app: Application) {
 
     if (code !== config.code) {
       res.statusCode = 401
-      return res.send({ code: 401, message: 'invalid code' })
+      return res.send({ error: 'Invalid code.' })
     }
 
     const response = await userBalanceRepository.save(balance).catch(error => {
@@ -161,7 +161,7 @@ export async function UserRoutes (app: Application) {
 
     if (code !== config.code) {
       res.statusCode = 401
-      return res.send({ code: 401, message: 'invalid code' })
+      return res.send({ error: 'Invalid code.' })
     }
 
     const response = await userLevelRepository.save(level).catch(error => {
@@ -178,7 +178,7 @@ export async function UserRoutes (app: Application) {
 
     if (code !== config.code) {
       res.statusCode = 401
-      return res.send({ code: 401, message: 'invalid code' })
+      return res.send({ error: 'Invalid code.' })
     }
 
     const response = await userLinksRepository.save(links).catch(error => {
@@ -215,25 +215,25 @@ export async function UserRoutes (app: Application) {
 
     if (!user) {
       console.log('User doesn\'t exist.')
-      return res.send('User doesn\'t exist.')
+      return res.send({ error: 'User doesn\'t exist.' })
     }
 
     const accessToken = user.accessToken
 
     if (decodedApiToken.accessToken !== accessToken) {
       console.log(`Access Token was incorrect\n${decodedApiToken.accessToken} !== ${accessToken}`)
-      return res.send('Invalid JWT.')
+      return res.send({ error: 'Invalid JWT.' })
     }
 
-    const same = await userRepository.findOne({ where: { links: { steamId } } })
+    const same = await userSteamLinksRepository.findOne({ where: { userId: steamId } })
 
     if (same) {
       console.log('Account already linked.')
-      return res.send('Account is already linked.')
+      return res.send({ error: 'Account is already linked to another user.' })
     }
 
-    user.links.steamId = steamId
-    await userLinksRepository.save(user.links)
+    user.links.steam.userId = steamId
+    await userSteamLinksRepository.save(user.links.steam)
 
     console.log('Successful')
     return res.send('Successful')
@@ -274,15 +274,15 @@ export async function UserRoutes (app: Application) {
       return res.send('Invalid JWT.')
     }
 
-    const same = await userRepository.findOne({ where: { links: { githubId } } })
+    const same = await userGithubLinksRepository.findOne({ where: { username: githubId } })
 
     if (same) {
       console.log('Account already linked.')
-      return res.send('Account is already linked.')
+      return res.send({ error: 'Account is already linked to another user.' })
     }
 
-    user.links.githubId = githubId
-    await userLinksRepository.save(user.links)
+    user.links.github.username = githubId
+    await userGithubLinksRepository.save(user.links.github)
 
     console.log('Successful')
     return res.send('Successful')
