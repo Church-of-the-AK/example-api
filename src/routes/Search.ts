d8554cc8300e8edc38a7613fb4ddfd62924ef07b
Jason Bothel <shadowmanguyyt@gmail.com>
2018-10-09 17:25:36 -0400

    Ignore case

--- src/routes/Search.ts
+++ src/routes/Search.ts
@@ -12,7 +12,9 @@ export async function SearchRoutes (app: Application) {
       return res.send({ success: false, error: 'no_query' })
     }
 
-    const users = await userRepository.find({ where: { name: Like(`%${query}%`) }, take: 100 })
+    const users = await userRepository.createQueryBuilder()
+      .where('LOWER(name) LIKE LOWER(:query)', { query })
+      .getMany()
 
     return res.send(users)
   })
