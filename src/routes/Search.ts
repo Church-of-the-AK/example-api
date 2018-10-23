48a6a7d3a394fff1caffa8b638fac4b5f1b65584
Jason Bothel <shadowmanguyyt@gmail.com>
2018-10-22 20:12:43 -0400

    Search playlists

--- src/routes/Search.ts
+++ src/routes/Search.ts
@@ -1,9 +1,10 @@
 import { Application } from 'express'
 import { getRepository } from 'typeorm'
-import { User } from 'machobot-database'
+import { User, MusicPlaylist } from 'machobot-database'
 
 export async function SearchRoutes (app: Application) {
   const userRepository = getRepository(User)
+  const playlistRepository = getRepository(MusicPlaylist)
 
   app.get('/api/search/users', async (req, res) => {
     const query: string = req.query.query
@@ -13,6 +14,20 @@ export async function SearchRoutes (app: Application) {
     }
 
     const users = await userRepository.createQueryBuilder()
+      .where('LOWER(name) LIKE LOWER(:query)', { query: `%${query}%` })
+      .getMany()
+
+    return res.send(users)
+  })
+
+  app.get('/api/search/playlists', async (req, res) => {
+    const query: string = req.query.query
+
+    if (!query) {
+      return res.send({ success: false, error: 'no_query' })
+    }
+
+    const users = await playlistRepository.createQueryBuilder()
       .where('LOWER(name) LIKE LOWER(:query)', { query: `%${query}%` })
       .getMany()
 
