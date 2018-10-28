8a86eb45f3b26a9987e4adaf96dcbb7db671c1f9
Jason Bothel <shadowmanguyyt@gmail.com>
2018-10-28 12:51:28 -0400

    Some changes to the index file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import * as express from 'express'
-import * as bodyParser from 'body-parser'
+import { urlencoded, json } from 'body-parser'
 import * as morgan from 'morgan'
-import * as https from 'https'
+import { createServer } from 'https'
 import { readFileSync } from 'fs'
-import * as database from 'machobot-database'
 import { db } from './config/config'
 import { createConnection } from 'typeorm'
 import { route } from './routes'
@@ -11,8 +10,8 @@ import { route } from './routes'
 const app = express()
 const port = 8000
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(urlencoded({ extended: true }))
+app.use(json())
 app.use(morgan(':method :url :status :res[content] - :response-time ms'))
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -22,7 +21,7 @@ app.use(function (req, res, next) {
 connect().then(connection => {
   route(app)
 
-  https.createServer({
+  createServer({
     key: readFileSync('/etc/letsencrypt/live/www.macho.ninja/privkey.pem'),
     cert: readFileSync('/etc/letsencrypt/live/www.macho.ninja/fullchain.pem')
   }, app).listen(port, function () {
@@ -38,8 +37,7 @@ async function connect () {
     username: db.user,
     password: db.password,
     database: db.database,
-    entities: [ database.User, database.UserBalance, database.UserLevel, database.UserLinks, database.UserGithubLinks, database.UserSteamLinks,
-      database.Guild, database.GuildSettings, database.MusicPlaylist, database.MusicSong, database.GuildTag ]
+    entities: [ 'node_modules/machobot-database/out/type/**/*.js' ]
   })
 
   await connection.synchronize()
